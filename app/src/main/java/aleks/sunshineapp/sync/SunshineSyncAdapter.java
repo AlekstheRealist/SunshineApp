c2d3a4c1a8dfa0688fed963d21c1738329d4470a
Aleks Real <aleksanderreal@gmail.com>
2017-01-12 16:47:46 -0800

    Updated color constants

--- app/src/main/java/aleks/sunshineapp/sync/SunshineSyncAdapter.java
+++ app/src/main/java/aleks/sunshineapp/sync/SunshineSyncAdapter.java
@@ -372,7 +372,7 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
 
                     NotificationCompat.Builder mBuilder =
                             new NotificationCompat.Builder(getContext())
-                                    .setColor(resources.getColor(R.color.sunshine_light_blue))
+                                    .setColor(resources.getColor(R.color.primary_light))
                                     .setSmallIcon(iconId)
                                     .setLargeIcon(largeIcon)
                                     .setContentTitle(title)
